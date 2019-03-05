5c84ffcd821beee1befaf0794ddc7f06c60c5bfa
Oseid <oseid.aldary@gmail.com>
2019-03-05 14:23:57 +0200

    change to use requests module instead of

--- ismail.py
+++ ismail.py
@@ -15,11 +15,11 @@ wi="\033[1;37m"
 #
 #Libraries#
 try:
-    import mechanize,socket,optparse; from os import system as sy
+    from os import system as sy, path; import requests,socket,optparse 
     sy("")
 except ImportError:
-    print(rd+"\n["+yl+"!"+rd+"] "+yl+"Error: Please Install [ "+gr+"Mechanize"+yl+" ] "+rd+" !!!")
-    print(wi+"["+gr+"*"+wi+"] Use This Command:>"+gr+" pip install mechanize")
+    print(rd+"\n["+yl+"!"+rd+"] "+yl+"Error: Please Install [ "+gr+"Requests"+yl+" ] "+rd+" !!!"+wi)
+    print(wi+"["+gr+"*"+wi+"] Use This Command:>"+gr+" pip install requests"+wi)
     exit(1)
 #
 # Check Internet Connection #
@@ -34,26 +34,17 @@ def cnet():
 #
 #Check-Email-Function#
 #
+
 def ISMAIL(email):
-    if cnet() !=True:
-        print(rd+"\n["+yl+"!"+rd+"]"+yl+" Error: Please Check Your Internet Connection "+rd+"!!!")
-        exit(1)
     try:
-        br = mechanize.Browser()
-        br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-        br.set_handle_robots(False)
-        br.open("https://verifyemailaddress.com/")
-        br.select_form(nr=0)
-        br["email"]=email
-        res = br.submit()
-        if "is valid" in res.get_data():
+        data = {"email": email}
+        headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.24 (KHTML, like Gecko) RockMelt/0.9.58.494 Chrome/11.0.696.71 Safari/534.24'}
+        response = requests.post("https://verifyemailaddress.com/result", headers=headers, data=data).text
+        if "is valid" in response:
             print(wi+"["+gr+"+"+wi+"] Email["+gr+email+wi+"] STATUS["+gr+" Found "+wi+"]")
         else:
-            print(yl+"["+rd+"-"+yl+"] Email["+rd+email+yl+"] STATUS["+rd+" Not Found "+yl+"]")
-    except KeyboardInterrupt:
-        print(wi+" ")
-        exit(1)
-    except EOFError:
+            print(yl+"["+rd+"-"+yl+"] Email["+rd+email+yl+"] STATUS["+rd+" Not Found "+yl+"]"+wi)
+    except(KeyboardInterrupt,EOFError):
         print(wi+" ")
         exit(1)
 
@@ -85,44 +76,50 @@ def Main():
     parse.add_option('-f','-F','--file','--FILE', dest="Fmail",type="string")
     (opt,args) = parse.parse_args()
     if opt.Smail !=None:
+        if cnet() !=True:
+            print(rd+"\n["+yl+"!"+rd+"]"+yl+" Error: Please Check Your Internet Connection "+rd+"!!!"+wi)
+            exit(1)
         email = opt.Smail
-        print(" ")
+        if not email.strip() or "@" not in email:
+            print(yl+"\n["+rd+"!"+yl+"] Invalid Email["+rd+email+yl+"] STATUS["+rd+" SKIPPED "+yl+"]"+wi)
+            exit(1)
+        email = email.strip()
+        print(gr+"["+yl+"~"+gr+"]"+yl+" Checking....\n"+wi)
         ISMAIL(email)
     elif opt.Mmail !=None:
+        if cnet() !=True:
+            print(rd+"\n["+yl+"!"+rd+"]"+yl+" Error: Please Check Your Internet Connection "+rd+"!!!"+wi)
+            exit(1)
         many_email = opt.Mmail
-        print(" ")
+        print(gr+"["+yl+"~"+gr+"]"+yl+" Checking....\n"+wi)
         if ',' in many_email:
             emails = many_email.split(",")
         else:
-            print(yl+"\n["+rd+"!"+yl+"] Error: Please Use[ "+wi+","+yl+" ] For Split The Emails"+rd+" !!!")
+            print(yl+"\n["+rd+"!"+yl+"] Error: Please Use[ "+wi+","+yl+" ] To Split The Emails"+rd+" !!!"+wi)
             exit(1)
         try:
             for email in emails:
-                ISMAIL(email)
-        except KeyboardInterrupt:
-            print(wi+" ")
-            exit(1)
-        except EOFError:
-            print(wi+" ")
-            exit(1) 
-    elif opt.Fmail !=None:
-        emails_file = opt.Fmail
-        print(" ")
-        try:
-            fop = open(emails_file, "r")
-        except IOError:
-            print(yl+"\n["+rd+"!"+yl+"]"+yl+" No Such File: "+rd+emails_file+wi)
-            exit(1)
-        try:
-            for email in fop:
-                if not email.strip(): continue
+                if not email.strip() or "@" not in email: continue
                 email = email.strip()
                 ISMAIL(email)
-            fop.close()
-        except KeyboardInterrupt:
+        except (KeyboardInterrupt,EOFError):
             print(wi+" ")
             exit(1)
-        except EOFError:
+            
+    elif opt.Fmail !=None:
+        emails_file = opt.Fmail
+        print(gr+"["+yl+"~"+gr+"]"+yl+" Checking....\n"+wi)
+        if not path.isfile(emails_file):
+            print(yl+"\n["+rd+"!"+yl+"]"+rd+"Error"+yl+" No Such File: "+rd+emails_file+wi)
+            exit(1)
+        try:
+            with open(emails_file) as fop:
+                for email in fop:
+                    if not email.strip() or "@" not in email: continue
+                    email = email.strip()
+                    ISMAIL(email)
+            fop.close()
+        except (KeyboardInterrupt,EOFError):
             print(wi+" ")
             exit(1)
     else:
@@ -131,8 +128,10 @@ def Main():
 
 if __name__=="__main__":
     Main()
+
+
 ##############################################################
-##################### 		    ##########################
+#####################               ##########################
 ##################### END OF SCRIPT ##########################
 #####################               ##########################
 ##############################################################
